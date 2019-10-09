e781c6fed7e49e7c2d71dee5b867bfa5812b652d
Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
2019-10-09 13:36:00 +0200

    latest version from the playground greetings branch
    Signed-off-by: Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>

---
+++ scripts/greetings.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+#
+# This file is part of the Robot Learning Lab stack
+#
+# Copyright (C) 2018 Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import rospy
+from rll_move_client.client import RLLDefaultMoveClient
+
+
+def greetings(move_client):
+    joint_1 = 0.6
+    joint_2 = 1.832
+    joint_3 = -0.3
+    joint_4 = 2.01
+    joint_5 = -2
+    joint_6 = 0.0
+    joint_7 = 1.4835
+
+    success = move_client.move_joints(joint_1, joint_2, joint_3, joint_4,
+                                      joint_5, joint_6, joint_7)
+
+    if not success:
+        rospy.logerr("moving to first joint pos failed")
+        return False
+
+    joint_3 = 0.3
+    joint_5 = -0.7
+    success = move_client.move_joints(joint_1, joint_2, joint_3, joint_4,
+                                      joint_5, joint_6, joint_7)
+
+    if not success:
+        rospy.logerr("moving to second joint pos failed")
+        return False
+
+
+def execute(move_client):
+    for x in range(0, 5):
+        rospy.loginfo("greetings back and forth rotation %d/%d", x + 1, 5)
+        greetings(move_client)
+
+
+if __name__ == '__main__':
+    rospy.init_node('greetings')
+    client = RLLDefaultMoveClient(execute)
+    rospy.spin()
